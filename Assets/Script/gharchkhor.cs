2d917f272feb342688ae7cda476684e242b2fe6f
sahar sakhaiee <100339904+shrsakhaiee@users.noreply.github.com>
2022-06-07 23:25:04 +0430

    Add files via upload

---
+++ Assets/Script/gharchkhor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gharchkhor : MonoBehaviour
+{
+    private Rigidbody2D mario;
+    public float speed = 5;
+    public float jump = 5;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mario = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float s = Input.GetAxisRaw("Horizontal");
+        mario.velocity = new Vector2(s*speed,mario.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+       mario.velocity += Vector2.up*jump;
+    
+        {
+            
+        }
+        
+        
+    }
+}
