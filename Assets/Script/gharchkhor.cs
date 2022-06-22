769c9d78e7311469bd63a2c8fd014307f2252c0a
sahar sakhaiee <100339904+shrsakhaiee@users.noreply.github.com>
2022-06-22 16:48:09 +0430

    updated!!

--- Assets/Script/gharchkhor.cs
+++ Assets/Script/gharchkhor.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class gharchkhor : MonoBehaviour
 {
     private Rigidbody2D mario;
-    public float speed = 5;
-    public float jump = 5;
-    
+    public float speed = 6;
+    public float jump = 4;
+    public int score;
+    public GameObject coin;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +22,16 @@ public class gharchkhor : MonoBehaviour
     {
         float s = Input.GetAxisRaw("Horizontal");
         mario.velocity = new Vector2(s*speed,mario.velocity.y);
-
         if(Input.GetKeyDown(KeyCode.UpArrow))
-       mario.velocity += Vector2.up*jump;
-    
+            mario.velocity += Vector2.up*jump;
+    }
+    void  OnCollisionEnter2D(Collision2D coin)
+    {
+        if (coin.gameObject.name.StartsWith("coin"))
         {
-            
+            Destroy (coin.gameObject);
+            score +=1;
+            Debug.Log(score);
         }
-        
-        
     }
 }
